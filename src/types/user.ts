3b8eb75644b91bd58cd52ca33224c0c221eb81c0
Gabriel Leal <gabrielleal@gabriels-macbook-pro.home>
2026-01-26 11:35:55 +0100

    Fix User type to match API response

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,13 +1,18 @@
+export interface UserGuild {
+  id: string
+  permissions: number
+}
+
 export interface User {
-  id: string;
-  username: string;
-  discriminator?: string;
-  avatar: string | null;
-  email?: string;
+  id: string
+  username: string
+  avatar: string | null
+  guilds: UserGuild[]
+  active_sessions: number
 }
 
 export interface Session {
-  user: User;
-  token: string;
-  expiresAt: string;
+  user: User
+  token: string
+  expiresAt: string
 }
